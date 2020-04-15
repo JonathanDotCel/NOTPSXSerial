c719064910759b4d973800fa8c64bbb7dec61045
sicklebrick@gmail.com <61208676+JonathanDotCel@users.noreply.github.com>
2020-04-15 19:18:04 +0100

    Initial commit

---
+++ build.bat
@@ -0,0 +1,20 @@
+cls
+
+REM Both use the microsoft CSC under windows, so it doesn't really matter
+REM Wherever csc.exe is, either in your Vis Studio folder, win folder, mono, etc
+
+REM set path=%PATH%;c:\windows\Microsoft.NET\Framework\blahblahlbah
+REM set path=%PATH%;C:\ins\cs2017comm\MSBuild\15.0\Bin\Roslyn
+REM set path=%PATH%;C:\Program Files (x86)\Mono\bin\
+
+set path=%PATH%;C:\Program Files (x86)\Mono\bin\
+
+cls
+
+del NOTPSXSERIAL.EXE
+del NoPS.EXE
+pause
+csc NOTPSXSERIAL.cs -out:NoPS.EXE
+
+pause
+

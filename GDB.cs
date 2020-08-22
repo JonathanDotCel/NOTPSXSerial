cde96ceda5fc315434a738a35d30036ac9bb29aa
jonathandotcel <wopstickle@gmail.com>
2020-08-22 09:58:44 +0100

    Exception cause decoding

--- GDB.cs
+++ GDB.cs
@@ -1,5 +1,8 @@
 using System;
 using System.IO.Ports;
+
+using System.Net.Sockets;
+using System.Net;
 using System.Threading;
 
 
@@ -50,6 +53,8 @@ public class GDB{
 
 	public const int TCB_LENGTH_BYTES = (int)GPR.COUNT * 4;
 
+	public static Socket socket;
+
     public static void Init(){
 		
 		Console.WriteLine( "Checking if Unirom is in debug mode..." );
@@ -68,11 +73,28 @@ public class GDB{
 
 		Console.WriteLine( "Monitoring sio..." );
 
-		MonitorSIO();
+		InitSocket();
 
-		
+		MonitorSIOAndSocket();
 
     }
+
+	private static void InitSocket(){
+		
+		IPHostEntry ipHostInfo = Dns.GetHostEntry( Dns.GetHostName() );
+		Console.WriteLine( "Info: " + ipHostInfo + " " + ipHostInfo.Aliases[0] + " " + ipHostInfo.AddressList[0] );
+
+		IPAddress ip = ipHostInfo.AddressList[0];
+
+		IPEndPoint localEndpoint = new IPEndPoint( ip, 3333 );
+
+		socket = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
+
+		socket.Bind( localEndpoint );
+
+		socket.Listen( 2 );
+
+	}
 
 	public static bool GetRegs(){
 		
@@ -129,9 +151,47 @@ public class GDB{
 			}
 		}
 		Console.WriteLine();
+
+		UInt32 cause = tcb.regs[ (int)GPR.caus ] >> 2;
+		
+		switch ( cause ) {
+			case 0x04:
+				Console.WriteLine("AdEL - Data Load or instr fetch (0x{0})\n", cause);
+				break;
+			case 0x05:
+				Console.WriteLine("AdES - Data Store (unaligned?) (0x{0})\n", cause);
+				break;
+			case 0x06:
+				Console.WriteLine("IBE - Bus Error on instr fetch (0x{0})\n", cause);
+				break;
+			case 0x07:
+				Console.WriteLine("DBE - Bus Error on data load/store (0x{0})\n", cause);
+				break;
+			case 0x08:
+				Console.WriteLine("SYS - Unconditional Syscall (0x{0})\n", cause);
+				break;
+			case 0x09:
+				Console.WriteLine("BP - Break! (0x{0})\n", cause);
+				break;
+			case 0x0A:
+				Console.WriteLine("RI - Reserved Instruction (0x{0})\n", cause);
+				break;
+			case 0x0B:
+				Console.WriteLine("CpU - Coprocessor unavailable (0x{0})\n", cause);
+				break;
+			case 0x0C:
+				Console.WriteLine("Ov - Arithmetic overflow (0x{0})\n", cause);
+				break;
+
+			default:
+				Console.WriteLine("Code {0}!\n", cause);
+				break;
+		}
+		
+
 	}
 
-    public static void MonitorSIO(){
+    public static void MonitorSIOAndSocket(){
         
         string responeBuffer = ":)";
 
